1a3b8d81b5edccbf0ba08546b92335107636832a
xrhgaming94-lab <xrhgaming94@gmail.com>
2026-05-25 10:23:25 +0530

    Update account credentials for region 'IND'

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def get_account_credentials(region: str) -> str:
     r = region.upper()
 
     if r == "IND":
-        return "uid=4569404695&password=RAGHAVLIKESBOT_RAGHAV_2THCG"
+        return "uid=4673260517&password=S_STAR_U1QV6"
 
     elif r in {"BR", "US", "SAC", "NA"}:
         return "uid=4438226807&password=10007207D207D6FE0D61FD0AF71047F51466E747B6F10928DB13E9F2F25446B7"
